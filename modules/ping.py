24156d0dfcd70bd4e91317ce5fb8dfa384293594
Jordan Kinsley <jordan@jordantkinsley.org>
2012-09-11 09:56:31 -0600

    Switched from the actual Unicode byte to the \x01 string representation

--- modules/ping.py
+++ modules/ping.py
@@ -20,47 +20,47 @@ def hello(phenny, input):
 hello.rule = r'(?i)(hi|hello|hey) (I|Bli|Pi)nkie(Pie)?(Bot)?\b'
 
 def sniff(phenny, input): 
-    sniffresponse = random.choice(('Hey! Do I know you?', 'Umm...', 'Do I smell funny - like \'haha\' funny or \'funny\' funny?', 'EEP!', 'ACTION prods ' + input.nick + '\'s nose.'))
+    sniffresponse = random.choice(('Hey! Do I know you?', 'Umm...', 'Do I smell funny - like \'haha\' funny or \'funny\' funny?', 'EEP!', '\x01ACTION prods ' + input.nick + '\'s nose.\x01'))
     phenny.say(sniffresponse)
-sniff.rule = r'(?i)(ACTION sniffs (I|Bli|Pi)nkie(Pie)?(Bot)?)'
+sniff.rule = r'(?i)(\x01ACTION sniffs (I|Bli|Pi)nkie(Pie)?(Bot)?)'
 
 def grope(phenny, input): 
-    groperesponse = random.choice(('I love hugs!', 'ACTION hugs ' + input.nick + ' SUPER hard.', 'ACK!', 'Hahaha, you suprised me, ' + input.nick + '!', 'Gasp!'))
+    groperesponse = random.choice(('I love hugs!', '\x01ACTION hugs ' + input.nick + ' SUPER hard.\x01', 'ACK!', 'Hahaha, you suprised me, ' + input.nick + '!', 'Gasp!'))
     phenny.say(groperesponse)
-grope.rule = r'(?i)(ACTION runs over and squeezes (I|Bli|Pi)nkie(Pie)?(Bot)?)'
+grope.rule = r'(?i)(\x01ACTION runs over and squeezes (I|Bli|Pi)nkie(Pie)?(Bot)?)'
 
 def flirt(phenny, input): 
     attractresponse = '.'
     if input.nick in saucyreplies:
-        attractresponse = random.choice(('Well, hey there, cutie. ;)','Such a flirt!','ACTION pounces ' + input.nick + '.','ACTION winks at ' + input.nick + '.'))
+        attractresponse = random.choice(('Well, hey there, cutie. ;)','Such a flirt!','\x01ACTION pounces ' + input.nick + '.\x01','\x01ACTION winks at ' + input.nick + '.\x01'))
     else: 
         attractresponse = random.choice(('I don\'t like you that way...', 'Sorry, I\'m not like that.', 'Flattering, but no.', 'Are you trying to flirt with me?', 'I\'d rather just be friends.', '...'))
     phenny.say(attractresponse)
-flirt.rule = r'(?i)(ACTION winks at (I|Bli|Pi)nkie(Pie)?(Bot)?)'
+flirt.rule = r'(?i)(\x01ACTION winks at (I|Bli|Pi)nkie(Pie)?(Bot)?)'
 
 def lick(phenny, input): 
     lickresponse = '.'
     if input.nick in saucyreplies:
-        lickresponse = random.choice(('ACTION blushes.', 'ACTION pounces ' + input.nick + '.', 'ACTION steals ' + input.nick + '\'s virginity.', 'Hee... We better take this elsewhere...','Ooooh!'))
+        lickresponse = random.choice(('\x01ACTION blushes.\x01', '\x01ACTION pounces ' + input.nick + '.\x01', '\x01ACTION steals ' + input.nick + '\'s virginity.\x01', 'Hee... We better take this elsewhere...','Ooooh!'))
     else:
-        lickresponse = random.choice(('...', 'What do I taste like? I want to know!', 'Ah!', 'Eep!', 'That tickles!', 'ACTION licks ' + input.nick + ' right back.', 'I don\'t like you that way...', 'Eww...','Waugh...', 'Ah!', '!', 'ACTION blushes.', 'ACTION pounces ' + input.nick + '.', 'ACTION steals ' + input.nick + '\'s virginity.', 'Hehe...'))
+        lickresponse = random.choice(('...', 'What do I taste like? I want to know!', 'Ah!', 'Eep!', 'That tickles!', '\x01ACTION licks ' + input.nick + ' right back.\x01', 'I don\'t like you that way...', 'Eww...','Waugh...', 'Ah!', '!', '\x01ACTION blushes.\x01', '\x01ACTION pounces ' + input.nick + '.\x01', '\x01ACTION steals ' + input.nick + '\'s virginity.\x01', 'Hehe...'))
     phenny.say(lickresponse)
-lick.rule = r'(?i)(ACTION licks (I|Bli|Pi)nkie(Pie)?(Bot)?)'
+lick.rule = r'(?i)(\x01ACTION licks (I|Bli|Pi)nkie(Pie)?(Bot)?)'
 
 def growl(phenny, input): 
     # TODO: add more responses
-    growlresponse = random.choice(('Silly puppy!', 'ACTION gives ' + input.nick + ' a slice of cake.', 'What did I do?', 'ACTION growls back.', 'Something wrong?'))
+    growlresponse = random.choice(('Silly puppy!', '\x01ACTION gives ' + input.nick + ' a slice of cake.\x01', 'What did I do?', '\x01ACTION growls back.\x01', 'Something wrong?'))
     phenny.say(growlresponse)
-growl.rule = r'(?i)(ACTION growls at (I|Bli|Pi)nkie(Pie)?(Bot)?)'
+growl.rule = r'(?i)(\x01ACTION growls at (I|Bli|Pi)nkie(Pie)?(Bot)?)'
 
 def kiss(phenny, input):
     kissresponse = '.'
     if input.nick in saucyreplies:
-        kissresponse = random.choice(('ACTION blushes.', 'ACTION pounces ' + input.nick + '.', 'ACTION steals ' + input.nick + '\'s virginity.', 'Hee... We better take this elsewhere...','Ooooh!','ACTION kisses ' + input.nick + ' back.','ACTION pushes ' + input.nick + ' to the ground and ravishes them.'))
+        kissresponse = random.choice(('\x01ACTION blushes.\x01', '\x01ACTION pounces ' + input.nick + '.\x01', '\x01ACTION steals ' + input.nick + '\'s virginity.\x01', 'Hee... We better take this elsewhere...','Ooooh!','\x01ACTION kisses ' + input.nick + ' back.\x01','\x01ACTION pushes ' + input.nick + ' to the ground and ravishes them.\x01'))
     else:
-        kissresponse = random.choice(('ACTION blushes','What was that for?','ACTION leaps into the air in suprise.', 'ACTION pushes ' + input.nick + ' away.', 'ACTION bucks ' + input.nick + ' in the chest. Hard.', 'ACTION giggles.'))
+        kissresponse = random.choice(('\x01ACTION blushes\x01','What was that for?','\x01ACTION leaps into the air in suprise.\x01', '\x01ACTION pushes ' + input.nick + ' away.\x01', '\x01ACTION bucks ' + input.nick + ' in the chest. Hard.\x01', '\x01ACTION giggles.\x01'))
     phenny.say(kissresponse)
-kiss.rule = r'(?i)(ACTION kisses (I|Bli|Pi)nkie(Pie)?(Bot)?)'
+kiss.rule = r'(?i)(\x01ACTION kisses (I|Bli|Pi)nkie(Pie)?(Bot)?)'
 
 def interjection(phenny, input): 
     phenny.say(input.nick + '!')
@@ -69,30 +69,30 @@ interjection.priority = 'high'
 interjection.thread = False
 
 def hugs(phenny, input):
-    hugresponse = random.choice(('ACTION hugs ' + input.nick + ' back.', 'ACTION giggles.', 'Thanks, ' + input.nick + ', that felt really good!', 'Hugs for everypony!'))
+    hugresponse = random.choice(('\x01ACTION hugs ' + input.nick + ' back.\x01', '\x01ACTION giggles.\x01', 'Thanks, ' + input.nick + ', that felt really good!', 'Hugs for everypony!'))
     phenny.say(hugresponse)
-hugs.rule = r'(ACTION)? (?i)(hugs) (?i)(I|Bli|Pi)nkie(Pie)?(Bot)?'
+hugs.rule = r'(\x01ACTION)? (?i)(hugs) (?i)(I|Bli|Pi)nkie(Pie)?(Bot)?'
 
 def tickles(phenny, input):
     tickleresponse = '.'
     if input.nick in saucyreplies:
-        tickleresponse = random.choice(('Hehehe! That tickles!', 'ACTION rolls on the floor, laughing.','EEP!','ACTION gigglesnorts','ACTION rolls ' + input.nick + ' over and tickles them back!','ACTION declares a tickle war!','ACTION giggles and pushes ' + input.nick + ' to the floor and kisses them.', 'ACTION giggles and takes ' + input.nick + '\'s hoof, and leads them to private room.'))
+        tickleresponse = random.choice(('Hehehe! That tickles!', '\x01ACTION rolls on the floor, laughing.\x01','EEP!','\x01ACTION gigglesnorts\x01','\x01ACTION rolls ' + input.nick + ' over and tickles them back!\x01','\x01ACTION declares a tickle war!\x01','\x01ACTION giggles and pushes ' + input.nick + ' to the floor and kisses them.\x01', '\x01ACTION giggles and takes ' + input.nick + '\'s hoof, and leads them to private room.\x01'))
     else:
-        tickleresponse = random.choice(('Hehehe! That tickles!', 'ACTION rolls on the floor, laughing.','EEP!','ACTION gigglesnorts','ACTION rolls ' + input.nick + ' over and tickles them back!','ACTION declares a tickle war!'))
+        tickleresponse = random.choice(('Hehehe! That tickles!', '\x01ACTION rolls on the floor, laughing.\x01','EEP!','\x01ACTION gigglesnorts\x01','\x01ACTION rolls ' + input.nick + ' over and tickles them back!\x01','\x01ACTION declares a tickle war!\x01'))
     phenny.say(tickleresponse)
-tickles.rule = r'(ACTION)? (?i)(tickles) (?i)(I|Bli|Pi)nkie(Pie)?(Bot)?'
+tickles.rule = r'(\x01ACTION)? (?i)(tickles) (?i)(I|Bli|Pi)nkie(Pie)?(Bot)?'
 
 def parties(phenny, input):
     # TODO: add more responses
-    partyresponse = random.choice(('Woo! A party!', 'ACTION invites the whole town to party!', 'ACTION grabs her emergency party supplies!', 'ACTION puts on some sweet jams!'))
+    partyresponse = random.choice(('Woo! A party!', '\x01ACTION invites the whole town to party!\x01', '\x01ACTION grabs her emergency party supplies!\x01', '\x01ACTION puts on some sweet jams!\x01'))
     phenny.say(partyresponse)
-parties.rule = r'(ACTION)? (?i)(parties with) (?i)(I|Bli|Pi)nkie(Pie)?(Bot)?'
+parties.rule = r'(\x01ACTION)? (?i)(parties with) (?i)(I|Bli|Pi)nkie(Pie)?(Bot)?'
 
 def smiles(phenny, input):
-    # ACTION smiles back! gets extra "weight" because it's a good response
-    smilesresponse = random.choice(('Aww, I love to see my friends smile!','ACTION smiles back!','#youtubelink','ACTION grins!','ACTION claps her hooves together!','ACTION smiles back!'))
+    # \x01ACTION smiles back!\x01 gets extra "weight" because it's a good response
+    smilesresponse = random.choice(('Aww, I love to see my friends smile!','\x01ACTION smiles back!\x01','#youtubelink','\x01ACTION grins!\x01','\x01ACTION claps her hooves together!\x01','\x01ACTION smiles back!\x01'))
     phenny.say(smilesresponse)
-smiles.rule = r'(ACTION)? (?i)smiles (?i)(at|with|to|because of) (?i)(I|Bli|Pi)nkie(Pie)?(Bot)?'
+smiles.rule = r'(\x01ACTION)? (?i)smiles (?i)(at|with|to|because of) (?i)(I|Bli|Pi)nkie(Pie)?(Bot)?'
 
 #TODO: create a "frowns" function with rule for "frownie" smilies and /me frowns.
 
@@ -105,8 +105,8 @@ thanks.rule = r'(?i)(Thank( you|s)?(,)? (I|Bli|Pi)nkie(Pie)?(Bot)?)'
 # TODO: add these actions and appropriate responses
 '''
 Actions to add: 
-(first to four stars get written, and any action with more than two at that time will be written as well)
-* pokes **
+(first to four stars get written, and any action with three or more at that time will be written as well)
+* pokes ****
 * cupcakes *
 * kicks *
 * good night *
@@ -120,7 +120,7 @@ Actions to add:
 * random hugs (triggered via private message) 
 '''
 
-# action string 'ACTION'
+# action string '\x01ACTION\x01'
 
 if __name__ == '__main__': 
 	print(__doc__.strip())
