9ef4a94f0c0db68d75015b42748597c32c6b2907
JordanKinsley <jordan@jordantkinsley.org>
2012-09-11 18:37:46 -0600

    Finished coding and squashed bugs in the party.py module

--- modules/party.py
+++ modules/party.py
@@ -27,7 +27,7 @@ def setup(self):
     self.party_conn.commit()
 setup.thread = False
 
-def check_nick(channel, nick):
+def check_nick(phenny, channel, nick):
     seen = False
     sqlitedata = {
         'channel': channel,
@@ -38,7 +38,8 @@ def check_nick(channel, nick):
     
     c = check_nick.conn.cursor()
     c.execute('''select nick from nicks where channel = :channel and nick = :nick;''', sqlitedata)
-    
+    if c.fetchone():
+        seen = True
     return seen
 check_nick.conn = None
 check_nick.thread = False
@@ -46,12 +47,11 @@ check_nick.thread = False
 def on_join(phenny, input):
     channel = input.sender
     nick = input.nick
-    if check_nick(channel, nick):
+    if check_nick(phenny, channel, nick):
         return
     else:
         party(phenny, input)
-        seen_nick(channel, nick)
-on_join.event = 'JOIN'
+        seen_nick(phenny, channel, nick)
 on_join.rule = r'.*'
 on_join.thread = False
 
@@ -63,25 +63,27 @@ def party(phenny, input):
     else:
         # Normal party!
         phenny.say('Hey! I haven\'t seen you around here before! That must mean you\'re new!')
-        phenny.action('brings out the party cannon for {0}!'.format(nick)
+        phenny.action(input.sender, 'brings out the party cannon for {0}!'.format(nick))
         phenny.say('So, here\'s your welcome party! Woo!')
 
 def vip_party(phenny, input):
     nick = input.nick
     phenny.say('Hey! I haven\'t seen you around here before! That must mean you\'re new!')
-    phenny.action('brings out the party cannon for {0}!'.format(nick)
+    phenny.action(input.sender, 'brings out the party cannon for {0}!'.format(nick))
     phenny.say('So, here\'s your welcome party! Woo!')
     phenny.msg(nick, 'And you get an extra special welcome!')
     phenny.msg(nick, '\x01ACTION licks her lips and pushes {0} to the ground, grinning above them. She then licks their cheek and nibbles lightly on their ear, giggling loudly.\x01'.format(nick))
-    phenny.msg(nick, 'Maybe next time we can a lot more fun. Sound good?')
+    phenny.msg(nick, 'Maybe next time we can have a lot more fun. Sound good?')
     return
 
-def seen_nick(channel, nick):
+def seen_nick(phenny, channel, nick):
     sqlitedata = {'channel': channel, 'nick': nick,}
     if not seen_nick.conn:
         seen_nick.conn = sqlite3.connect(phenny.party_db)
     
     c = seen_nick.conn.cursor()
-    #c.execute insert
+    c.execute('''insert or replace into nicks values(:channel, :nick);''', sqlitedata)
+    c.close()
+    seen_nick.conn.commit()
 seen_nick.thread = False
 seen_nick.conn = None
