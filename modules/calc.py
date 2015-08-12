d4732d292ab7bd5d3c472fb43b2f528e3bc30d71
Compu <hotwheels14901@me.com>
2015-08-12 14:51:26 -0400

    fixed the calc function

--- modules/calc.py
+++ modules/calc.py
@@ -27,32 +27,43 @@ subs = [
     ('mbps', '(megabits / second)')
 ]
 
-def c(phenny, input): 
-    """Google calculator."""
+def calculate(phenny, input): 
+    """Calculate things."""
     if not input.group(2):
         return phenny.reply("Nothing to calculate.")
     q = input.group(2)
     q = q.replace('\xcf\x95', 'phi') # utf-8 U+03D5
     q = q.replace('\xcf\x80', 'pi') # utf-8 U+03C0
-    uri = 'http://www.google.com/ig/calculator?q='
-    bytes = web.get(uri + web.quote(q))
-    parts = bytes.split('",')
-    answer = [p for p in parts if p.startswith('rhs: "')][0][6:]
+    q = q.replace('÷', '/')
+    q = web.quote(q)
+    uri = 'https://www.calcatraz.com/calculator/api?c=' + q
+    answer = web.get(uri)
     if answer: 
+        answerindex = 1
+        if (len(answer.split(";")) < 2):
+            answerindex = 0
+        answer = answer.split(";")[answerindex]
+        answer = answer.replace('  ','')
         #answer = ''.join(chr(ord(c)) for c in answer)
         #answer = answer.decode('utf-8')
-        answer = answer.replace('\\x26#215;', '*')
-        answer = answer.replace('\\x3c', '<')
-        answer = answer.replace('\\x3e', '>')
-        answer = answer.replace('<sup>', '^(')
-        answer = answer.replace('</sup>', ')')
-        answer = web.decode(answer)
-        phenny.say(answer)
-    else: phenny.say('Sorry, no result.')
-c.commands = ['c']
-c.example = '.c 5 + 3'
+        #answer = answer.replace('\\x26#215;', '*')
+        #answer = answer.replace('\\x3c', '<')
+        #answer = answer.replace('\\x3e', '>')
+        #answer = answer.replace('<sup>', '^(')
+        #answer = answer.replace('</sup>', ')')
+        #answer = web.decode(answer)
+        if re.compile('answer').match(answer):
+            return phenny.say('Sorry, no result.')
+        else:
+            return phenny.say(answer)
+    else: 
+        return phenny.say('Sorry, no result.')
+calculate.commands = ['c','calc','calculate']
+calculate.example = '.c 5 + 3'
 
-def py(phenny, input): 
+def py(phenny, input):
+    if not input.group(2):
+        return phenny.reply(".py what?") 
     query = input.group(2)
     uri = 'http://tumbolia.appspot.com/py/'
     answer = web.get(uri + web.quote(query))
