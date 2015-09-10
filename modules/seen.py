61199243e4c43497aa0b793f01f02b49a5bea699
Compu <hotwheels14901@me.com>
2015-09-10 16:02:02 -0400

    complete rewrite of seen.py from scratch
    now uses sqlite for seen info

--- modules/seen.py
+++ modules/seen.py
@@ -1,55 +1,123 @@
 #!/usr/bin/env python
 """
-seen.py - Phenny Seen Module
-Copyright 2008, Sean B. Palmer, inamidst.com
-Licensed under the Eiffel Forum License 2.
- 
-http://inamidst.com/phenny/
+seen.py - Phenny seen module
 """
- 
+import sqlite3 as lite
+import os
+import datetime
 import time
-from tools import deprecated
- 
-@deprecated
-def f_seen(self, origin, match, args):
-    """.seen <nick> - Reports when <nick> was last seen."""
-    if origin.sender == '#talis': return
-    if match.lastindex is None or match.lastindex < 2:
-        self.msg(origin.sender, "You didn't tell me who to look for, silly!")
-        return
-    nick = match.group(2).lower()
-    if origin.nick.lower() == nick :
-        self.msg(origin.sender, "Silly, that's you!")
-        return
-    if not hasattr(self, 'seen'):
-        return self.msg(origin.sender, '?')
-    if nick in self.seen:
-        channel, t = self.seen[nick]
-        t = time.strftime('%Y-%m-%d %H:%M:%S UTC', time.gmtime(t))
- 
-        msg = "I last saw %s at %s on %s" % (nick, t, channel)
-        self.msg(origin.sender, str(origin.nick) + ': ' + msg)
-    else: self.msg(origin.sender, "Sorry, I haven't seen %s around." % nick)
-f_seen.rule = (['seen'], r'(\S+)')
- 
-@deprecated
-def f_note(self, origin, match, args):
-    def note(self, origin, match, args):
-        if not hasattr(self.bot, 'seen'):
-            self.bot.seen = {}
-        if origin.sender.startswith('#'):
-            # if origin.sender == '#inamidst': return
-            self.seen[origin.nick.lower()] = (origin.sender, time.time())
- 
-        # if not hasattr(self, 'chanspeak'):
-        #     self.chanspeak = {}
-        # if (len(args) > 2) and args[2].startswith('#'):
-        #     self.chanspeak[args[2]] = args[0]
- 
-    try: note(self, origin, match, args)
-    except Exception as e: print(e)
-f_note.rule = r'(.*)'
-f_note.priority = 'low'
- 
-if __name__ == '__main__':
-    print(__doc__.strip())
+
+def db_connect(db):
+    return lite.connect(db, check_same_thread = False, detect_types=lite.PARSE_DECLTYPES)
+
+def setup(phenny):
+    seen_db = os.path.join(os.path.expanduser('~/.phenny'), 'seen.db')
+    seen_conn = db_connect(seen_db)
+    c = seen_conn.cursor()
+    c.execute('''create table if not exists seen(
+        nick    varchar(31) NOT NULL PRIMARY KEY,
+        channel varchar(31) NOT NULL,
+        message text,
+        event   varchar(10) NOT NULL,
+        time    TIMESTAMP DEFAULT CURRENT_TIMESTAMP NOT NULL
+        );''')
+    c.close()
+    seen_conn.commit()
+    seen_conn.close()
+setup.thread = False
+
+def smart_truncate(content):
+    suffix='...'
+    length=int(150)
+    if len(content) <= length:
+        return content
+    else:
+        return content[:length].rsplit(' ', 1)[0]+suffix
+
+def seen(phenny, input):
+    """Gives when a user was last seen."""
+    inputnick = input.group(2)
+
+    seen_db = os.path.join(os.path.expanduser('~/.phenny'), 'seen.db')
+    seen_conn = db_connect(seen_db)
+    conn = db_connect(seen_db)
+    c = conn.cursor()
+    query = (inputnick,)
+    c.execute("SELECT * FROM seen WHERE nick LIKE ?;", query)
+    resultsun = c.fetchall()
+    try:
+        results = resultsun[0]
+    except:
+        nick = None
+    
+    try:
+        nick = results[0]
+        channel = results[1]
+        message = results[2]
+        event = results[3]
+        seentimeun = results[4]
+    except:
+        nick = None
+    if not nick:
+        phenny.say("Sorry I haven't seen " + inputnick)
+    elif input.nick == inputnick:
+        phenny.say("Silly, that's you!")
+    else:
+        
+        seentime = seentimeun.strftime('%A %B %d, %G at %I:%M:%S %p GMT')
+        message = smart_truncate(message)
+        if event == "PRIVMSG":
+            phenny.say(nick + " was last seen in " + channel + ' saying "' + message + '" on ' + seentime)
+        elif event == "JOIN":
+            phenny.say(nick + " was last seen joining " + channel + " on " + seentime)
+        elif event == "PART":
+            phenny.say(nick + " was last seen leaving " + channel + ' with message "' + message + '" on ' + seentime)
+        elif event == "QUIT":
+            phenny.say(nick + ' was last seen quitting with message "' + message + '" on ' + seentime)
+    c.close()
+seen.commands = ['seen']
+seen.example = ".seen somenick"
+
+def seenstore(phenny, input, event):
+    nick = input.nick
+    channel = input.sender
+    if event == "JOIN":
+        message = None
+    else:
+        message = input.group()
+    seen_db = os.path.join(os.path.expanduser('~/.phenny'), 'seen.db')
+    seen_conn = db_connect(seen_db)
+    conn = db_connect(seen_db)
+    c = conn.cursor()
+    entry = (nick, channel, message, event)
+    c.execute("INSERT OR REPLACE INTO seen(nick, channel, message, event) VALUES(?, ?, ?, ?)", entry)
+    conn.commit()
+    c.close()
+    conn.close()
+
+def seenmsg(phenny, input):
+    event = "PRIVMSG"
+    seenstore(phenny, input, event)
+seenmsg.rule = r'(.*)'
+seenmsg.priority = 'low'
+
+#def seenjoin(phenny, input):
+#    event = "JOIN"
+#    seenstore(phenny, input, event)
+#seenjoin.event = 'JOIN'
+#seenjoin.rule = r'(.*)'
+#seenjoin.priority = 'low'
+
+#def seenquit(phenny, input):
+#    event = "QUIT"
+#    seenstore(phenny, input, event)
+#seenquit.event = 'QUIT'
+#seenquit.rule = r'(.*)'
+#seenquit.priority = 'low'
+
+#def seenpart(phenny, input):
+#    event = "PART"
+#    seenstore(phenny, input, event)
+#seenpart.event = 'PART'
+#seenpart.rule = r'(.*)'
+#seenpart.priority = 'low'
