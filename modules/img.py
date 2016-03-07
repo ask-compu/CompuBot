69b1f2a3955c1ff031cd5b68b1fcf597c7c8cceb
Compu <hotwheels14901@me.com>
2016-03-06 19:36:04 -0500

    added message if user gives no input for commands

--- modules/img.py
+++ modules/img.py
@@ -73,13 +73,15 @@ store.conn = None # these variables MUST be named with the function, other they 
 store.thread = False
 
 def add(phenny, input):
-    '''.add [<character>] <image url> - Adds an image URL for a given character. 
+    '''.add [<character>] <url> - Adds an image URL for a given character. 
     The URL must have http in it.'''
     if not input.sender.startswith('#'): 
         phenny.reply('This is a channel-only command. Please try again in a channel.')
     channel = input.sender
     nick = input.nick
     
+    if not input.group(2):
+        return phenny.say("Usage .add <name> <url>")
     # get the url from the input
     try: 
         character, url = input.group(2).split()
@@ -191,6 +193,9 @@ def delete(phenny, input):
     nick = input.nick
     character = input.group(2)
     
+    if not input.group(2):
+        return phenny.say("Usage .del <name>")
+    
     char_entry = get_character(phenny, character, channel)
     if char_entry is None:
         phenny.say("Sowwy, but I don't remember anything by that name! Are you sure that you stored it? Pwease don't be mad at me! :(")
@@ -222,6 +227,9 @@ def get(phenny, input):
     channel = input.sender
     character = input.group(2)
     
+    
+    if not input.group(2):
+        return phenny.say("Usage .get <name>")
     sqlitedata = {
         'channel': channel,
         'character': character,
